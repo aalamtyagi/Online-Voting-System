b62cd039dcd3018d8fa5ec879ffed305c4538ff0
AALAM GEER <aalamtyagi788@gmail.com>
2023-10-01 13:21:12 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,17 @@
+				<?php
+
+				$hostname="localhost";
+				$username= "root";
+				$password= "";
+				$database="db_evoting";
+					function test_input($data) {
+					  $data = trim($data);
+					  $data = stripslashes($data);
+					  $data = htmlspecialchars($data);
+					
+					  return $data;
+					}	
+
+								
+
+?>
